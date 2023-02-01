7872448e65665dfa2e4b86fb14eaf56491c0cf91
Abhishek Soni <asoni1@binghamton.edu>
2023-02-01 13:27:19 -0500

    Basic Spring Application

---
+++ 02-backend/sping-boot-library/src/main/java/com/abhisoni/spingbootlibrary/SpingBootLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.abhisoni.spingbootlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootLibraryApplication.class, args);
+	}
+
+}
---
+++ 02-backend/sping-boot-library/src/main/java/com/abhisoni/spingbootlibrary/config/MyDataRestConfig.java
@@ -0,0 +1,44 @@
+package com.abhisoni.spingbootlibrary.config;
+
+import com.abhisoni.spingbootlibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private String theAllowedOrigin = "http://localhost:3002";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
+
+        HttpMethod[] unsupportedmethods = {
+                HttpMethod.POST,
+                HttpMethod.PATCH,
+                HttpMethod.PUT,
+                HttpMethod.DELETE,
+        };
+
+        config.exposeIdsFor(Book.class);
+        
+        disableHttpMethods(Book.class, config, unsupportedmethods);
+
+        /* Configure CORS mapping */
+
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigin);
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] unsupportedmethods) {
+
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedmethods)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedmethods));
+
+    }
+
+}
